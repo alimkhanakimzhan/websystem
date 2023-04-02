9dbd61cc2d816baf85121180e1b693516e500a3f
Zakir Marajapov <zmarajapov@gmail.com>
2023-04-02 22:44:28 +0600

    сравнял отступы

--- career-intersections.php
+++ career-intersections.php
@@ -11,12 +11,13 @@ require_once "config.php";
 ?>
 
 <html>
-  <?php require_once(HOME_DIR.'/include/header.php') ?>
-  <body>
-    <div class="wrapper" style="">
-      <div class="container rounded bg-white mt-5 mb-5">
+<?php require_once(HOME_DIR.'/include/header.php') ?>
 
-        <?php
+<body>
+  <div class="wrapper" style="">
+    <div class="container rounded bg-white mt-5 mb-5">
+
+      <?php
         if(!isset($_GET['id'])) {
           header("location: person.php");
         } else {
@@ -49,84 +50,85 @@ require_once "config.php";
           ) co_workers ON co_workers.person_id != job_history.person_id
           WHERE job_history.person_id = $id
           AND job_history.organization_ID = $organization_ID
-          AND job_history.time_start_position BETWEEN '$start_period' AND '$end_period' ;
-
-
-          ")) {
+          AND job_history.time_start_position BETWEEN '$start_period' AND '$end_period' ;")) 
+          {
 
             $query->execute();
             $result = $query->get_result();
-            if ($row = $result->fetch_assoc()) {
-                    if (! empty($row)) {
-                      echo '
-                        <table class="table table-responsive">
-                          <thead>
-                            <tr>
-                              <th>Аватар</th>
-                              <th>Фамилия</th>
-                              <th>Имя</th>
-                              <th>Должность</th>
-                              <th>Период работы</th>
-                            </tr>
-                          </thead>
-                          <tbody>
-                      ';
-                        while ($row = $result->fetch_assoc()) {
-                            $FirstName = $row['CoWorkerFirstName'];
-                            $LastName = $row['CoWorkerLastName'];
-                            $Position = $row['position_name'];
-                            $coworker_id = $row['person_id'];
-                            $Photo = $row['Photo'];
-                            $coworker_start_period = $row['time_start_position'];
-                            $coworker_end_period = $row['time_end_position'];
-                            if ($coworker_end_period == "0000-00-00") {
-                            $coworker_end_period = "текущий момент";
-                            }
+            if ($row = $result->fetch_assoc()) 
+            {
+                if (! empty($row)) 
+                {
 
-                            echo
-                            '<tr class="alert" role="alert">
-                              <td class="d-flex align-items-center">
-                                <div class="img" style="background-image: url('.$Photo.');"></div>
-                              </td>
-                              <td class="">
-                                <a href="person-single.php?id='.$coworker_id.'"><span>'.$LastName.'</span></a>
-                              </td>
-                              <td class="">
-                                <a href="person-single.php?id='.$coworker_id.'"><span>'.$FirstName.'</span></a>
-                              </td>
-                              <td class="">
-                                <span>'.$Position.'</span>
-                              </td>
-                              <td class="">
-                                <span>'.$coworker_start_period.' по '.$coworker_end_period.' </span>
-                              </td>
-                            </tr>';
-                          }
-                          echo '
-                          </tbody>
-                          </table>
+                  echo '
+                    <table class="table table-responsive">
+                      <thead>
+                        <tr>
+                          <th>Аватар</th>
+                          <th>Фамилия</th>
+                          <th>Имя</th>
+                          <th>Должность</th>
+                          <th>Период работы</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                  ';
 
-                          ';
-                        }
-                       }else {
-                         header("Location:  person-single.php?id=$id");
-                         exit;
-                    }
-
+                  while ($row = $result->fetch_assoc()) {
+                      $FirstName = $row['CoWorkerFirstName'];
+                      $LastName = $row['CoWorkerLastName'];
+                      $Position = $row['position_name'];
+                      $coworker_id = $row['person_id'];
+                      $Photo = 'images/avatars/persons/' . $row["Photo"];
+                      $coworker_start_period = $row['time_start_position'];
+                      $coworker_end_period = $row['time_end_position'];
+                      if ($coworker_end_period == "0000-00-00") {
+                      $coworker_end_period = "текущий момент";
+                      }
+                      echo
+                      '<tr class="alert" role="alert">
+                        <td class="d-flex align-items-center">
+                          <div class="img" style="background-image: url('.$Photo.');"></div>
+                        </td>
+                        <td class="">
+                          <a href="person-single.php?id='.$coworker_id.'"><span>'.$LastName.'</span></a>
+                        </td>
+                        <td class="">
+                          <a href="person-single.php?id='.$coworker_id.'"><span>'.$FirstName.'</span></a>
+                        </td>
+                        <td class="">
+                          <span>'.$Position.'</span>
+                        </td>
+                        <td class="">
+                          <span>'.$coworker_start_period.' по '.$coworker_end_period.' </span>
+                        </td>
+                      </tr>';
+                  }
+                  echo '
+                  </tbody>
+                  </table>
+                  ';
                 }
+            }else{
+                header("Location:  person-single.php?id=$id");
+                exit;
+            }
+
+          }
 
         }
 
 
         ?>
 
-          </div>
     </div>
+  </div>
 
-    <?php
+  <?php
     $page = "person_page";
     require_once(HOME_DIR.'/include/navmenu.php');
     ?>
 
-  </body>
+</body>
+
 </html>
