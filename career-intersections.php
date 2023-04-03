5c4ed2182da3c511012b9ef64e3e81169ae8ed0e
Temirlan-Tazhibayev <79756276+Temirlan-Tazhibayev@users.noreply.github.com>
2023-04-03 11:58:20 +0600

    career-intersections
    graph and table work correctly

--- career-intersections.php
+++ career-intersections.php
@@ -23,6 +23,7 @@ require_once "config.php";
       $start_period = $_GET['start_period'];
       $end_period = $_GET['end_period'];
       $intersections = [];
+      $counter = 1;
 
       if($query = $db->prepare("
       SELECT persons.FirstName, persons.LastName, job_history.position_name, co_workers.FirstName as CoWorkerFirstName,
@@ -40,23 +41,33 @@ require_once "config.php";
       ) co_workers ON co_workers.person_id != job_history.person_id
       WHERE job_history.person_id = $id
       AND job_history.organization_ID = $organization_ID
-      AND job_history.time_start_position BETWEEN '$start_period' AND '$end_period' ;")) 
+      AND job_history.time_start_position BETWEEN '$start_period' AND '$end_period' ORDER BY co_workers.time_start_position ASC;")) 
       {
         $query->execute();
         $result = $query->get_result();
-          if($result->num_rows > 0) {
-            while ($row = $result->fetch_assoc()) {
-              if (! empty($row)) {
-                $intersections[] = [
-                  'id' => $row['person_id'],
-                  'content' => $row['CoWorkerFirstName'] . ' ' . $row['CoWorkerLastName'],
-                  'PositionName' => $row['position_name'],
-                  'time_start_position' => $row['time_start_position'],
-                  'time_end_position' => $row['time_end_position']
-                ];
-              }
+        if($result->num_rows > 0) {
+          while ($row = $result->fetch_assoc()) {
+            if (! empty($row)) {
+
+              if ($row['time_end_position']=="0000-00-00"){
+                $date = new DateTime(); // create a new DateTime object with the current date and time
+                $end_date = $date->format('Y-m-d');
+              } else {
+                $end_date = $row['time_end_position'];
+              };
+
+              $intersections[] = [
+                'id' => $counter,
+                'content' => $row['CoWorkerFirstName'] . ' ' . $row['CoWorkerLastName'] . '<br>' . $row['position_name'],
+                'start' => $row['time_start_position'],
+                'end' => $end_date
+              ];
+              
+
+              $counter+=1;
             }
           }
+        }
       }
     ?>
 
@@ -66,9 +77,33 @@ require_once "config.php";
 
       // Create a DataSet (allows two way data-binding)
       var items = new vis.DataSet(<?php echo json_encode($intersections, JSON_UNESCAPED_UNICODE) ?>);
+      // Assign a unique color to each item
+      var colors = new vis.DataSet();
+      var colorCount = 0;
+      items.forEach(function(item) {
+        var color = {id: item.id, background: 'rgb(' + colorCount*30 + ',' + colorCount*20 + ',' + colorCount*10 + ')'};
+        colors.add(color);
+        colorCount++;
+      });
 
       // Configuration for the Timeline
-      var options = {};
+      var options = {
+        // width: '1000px',
+        height: '350px',
+        orientation: 'top',
+        stack: true,
+        align: 'left',
+        tooltip: {
+          followMouse: true,
+          overflowMethod: 'flip',
+          delay: 200,
+          template: function(item) {
+            return item.content;
+          }
+        }
+
+      };
+      
 
       // Create a Timeline
       var timeline = new vis.Timeline(container, items, options);
@@ -107,9 +142,8 @@ require_once "config.php";
           ) co_workers ON co_workers.person_id != job_history.person_id
           WHERE job_history.person_id = $id
           AND job_history.organization_ID = $organization_ID
-          AND job_history.time_start_position BETWEEN '$start_period' AND '$end_period' ;")) 
+          AND job_history.time_start_position BETWEEN '$start_period' AND '$end_period' ORDER BY co_workers.time_start_position ASC ;")) 
           {
-
             $query->execute();
             $result = $query->get_result();
             if ($row = $result->fetch_assoc()) 
@@ -130,6 +164,24 @@ require_once "config.php";
                       </thead>
                       <tbody>
                   ';
+                  echo
+                      '<tr class="alert" role="alert">
+                        <td class="d-flex align-items-center">
+                          <div class="img" style="background-image: url('.'images/avatars/persons/' . $row["Photo"].');"></div>
+                        </td>
+                        <td class="">
+                          <a href="person-single.php?id='.$row['person_id'].'"><span>'.$row['CoWorkerLastName'].'</span></a>
+                        </td>
+                        <td class="">
+                          <a href="person-single.php?id='.$row['person_id'].'"><span>'.$row['CoWorkerFirstName'].'</span></a>
+                        </td>
+                        <td class="">
+                          <span>'.$row['position_name'].'</span>
+                        </td>
+                        <td class="">
+                          <span>'.$row['time_start_position'].' по '.$row['time_end_position'].' </span>
+                        </td>
+                      </tr>';
 
                   while ($row = $result->fetch_assoc()) {
                       $FirstName = $row['CoWorkerFirstName'];
@@ -166,26 +218,19 @@ require_once "config.php";
                   </table>
                   ';
                 }
-            }else{
+            }
+            else{
                 header("Location:  person-single.php?id=$id");
                 exit;
             }
-
           }
-
         }
-
-
         ?>
-
+      </div>
     </div>
-  </div>
-
-  <?php
+    <?php
     $page = "person_page";
     require_once(HOME_DIR.'/include/navmenu.php');
     ?>
-
-</body>
-
+  </body>
 </html>
